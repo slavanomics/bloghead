25cd4f138a88b3fc2a0b5cd9f99e3fd0955c3caf
raddcircles <raddcircles@gmail.com>
2021-09-04 18:46:17 -0400

    add database connection testing

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 from flask import Flask, render_template, redirect, flash
 from flask_login import login_required, current_user
+from flask_sqlalchemy import SQLAlchemy
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField,IntegerField
 from wtforms.validators import DataRequired, NumberRange
 from util import is_setup
-from os import urandom
+from os import urandom, path
 app = Flask(__name__)
 app.debug = True
 app.config['SECRET_KEY'] = urandom(32)
 
+if path.exists('setup.ini'):
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+
+db = SQLAlchemy(app)
 class SetupForm(FlaskForm):
     dbuser = StringField('User', validators=[DataRequired()])
     dbpass = PasswordField('Password', validators=[DataRequired()])
@@ -30,10 +35,15 @@ def index():
 def setup():
     form = SetupForm()
     if form.validate_on_submit():
-        if True == True:
-            flash("test")
+        try:
+            # test sql connection before comitting to file
+            # construct sqlalchemy uri
+            app.config['SQLALCHEMY_DATABASE_URI'] = f'mariadb+mariadbconnector://{form.dbuser.data}:{form.dbpass.data}@{form.dbhost.data}:{form.dbport.data}/{form.dbname.data}' # TODO@slavanomics add select in form and make this work with engines other than mariadb.
+            db.create_all()
+        except Exception as e:
+            flash(str(e))
             return render_template('setup.html',form=form)
-        return redirect('/success')
+        return "t"
     # do setup logic
     return render_template('setup.html',form=form)
 
