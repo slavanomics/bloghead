722842794c25bc07228c9faa10835b3a6093471f
raddcircles <raddcircles@gmail.com>
2021-09-04 18:04:01 -0400

    make setup form

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, flash
 from flask_login import login_required, current_user
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField,IntegerField
+from wtforms.validators import DataRequired, NumberRange
 from util import is_setup
 from os import urandom
 app = Flask(__name__)
@@ -11,7 +11,13 @@ app.debug = True
 app.config['SECRET_KEY'] = urandom(32)
 
 class SetupForm(FlaskForm):
-    name = StringField('name', validators=[DataRequired()])
+    dbuser = StringField('User', validators=[DataRequired()])
+    dbpass = PasswordField('Password', validators=[DataRequired()])
+    dbhost = StringField('Host', validators=[DataRequired()])
+    dbport = IntegerField('Port', validators=[DataRequired(),NumberRange(0,65535,"That's an impossible port!")])
+    dbname = StringField('Database Name',validators=[DataRequired()])
+    username = StringField('Username',validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
 
 toolbar = DebugToolbarExtension(app)
 
@@ -24,6 +30,9 @@ def index():
 def setup():
     form = SetupForm()
     if form.validate_on_submit():
+        if True == True:
+            flash("test")
+            return render_template('setup.html',form=form)
         return redirect('/success')
     # do setup logic
     return render_template('setup.html',form=form)
