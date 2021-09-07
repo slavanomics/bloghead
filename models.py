6610b2ff0ab5924cd94005e013edab495c2ead80
raddcircles <raddcircles@gmail.com>
2021-09-07 15:05:16 -0400

    add password to user model

--- models.py
+++ models.py
@@ -5,4 +5,5 @@ db = SQLAlchemy()
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String, nullable=False)
